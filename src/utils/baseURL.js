8ea675d4d0c8124617ad56eabef0b5add4c914e8
bilal-alaabadi <mdbilalnizwa@gmail.com>
2025-07-30 15:52:46 +0400

    Update baseURL.js

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -2,5 +2,5 @@
 //     return 'http://localhost:5000';//رابط الباك اند بدل 5000
 // }
 export const getBaseUrl = () => {
-    return 'http://localhost:5000';//رابط الباك اند بدل 5000
+    return 'https://henaporgd-b.vercel.app';//رابط الباك اند بدل 5000
 }
