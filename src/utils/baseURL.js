1793931ed55836fa5b6175d776136a2c587af3ec
bilal-alaabadi <mdbilalnizwa@gmail.com>
2025-08-12 00:30:17 +0400

    وصف التغييرات هنا

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -2,5 +2,5 @@
 //     return 'http://localhost:5000';//رابط الباك اند بدل 5000
 // }
 export const getBaseUrl = () => {
-    return 'http://localhost:5000';//رابط الباك اند بدل 5000 رجع
+    return 'https://liv-b.vercel.app';//رابط الباك اند بدل 5000 رجع
 } 
